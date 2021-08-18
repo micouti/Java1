13ea8ab4b0ca084090896cf86541a7a972baa358
micouti <mi.coutinho8@hotmail.com>
2021-08-18 17:19:25 -0300

    Adicionando JS na página guia e na página pessoal

---
+++ Desktop/Nova pasta/Milena/work/Aula1307/src/br/com/generation/collections/Collections1.java
@@ -0,0 +1,10 @@
+package br.com.generation.collections;
+
+public class Collections1 {
+
+	public static void main(String[] args) {
+		
+
+	}
+
+}
---
+++ Desktop/Nova pasta/Milena/work/Aula1307/src/br/com/generation/collections/TestaColletions.java
@@ -0,0 +1,10 @@
+package br.com.generation.collections;
+
+public class TestaColletions {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
